bb4eca9d8dc57b5d5844d8d602325012217b5988
cjballard <christopher.balard@mapbox.com>
2019-01-24 10:54:38 -0500

    Ratings, timer, start of modal and a bunch of code comments

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 /*
  * Create a list that holds all of your cards
+ Other global variables for stars, modal, arry for holding toggled cards, moves and timers.
  */
 
 const deck = document.querySelector('.deck');
+const stars = document.querySelectorAll('ul.stars li');
+const modal = document.querySelector('.modal');
 let toggledCards = [];
 let moves = 0;
+let timerStop = true;
+let timerId;
 
 /*
  * Display the cards on the page
@@ -29,7 +34,6 @@ function shuffle(cards) {
 };
 
 
-
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -41,57 +45,93 @@ function shuffle(cards) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
 */
 
-//timer 
+// Returning star ratings based on amount of moves a player makes.
+function getScore () {
+    //If given a certain number of moves, remove child stars from page.
+    if (moves > 8 && moves < 14) {
+        document.querySelector('ul.stars').removeChild(stars[0]);
+    }
+    else if (moves >= 14 && moves < 18) {
+        document.querySelector('ul.stars').removeChild(stars[0, 1]);
+    }
+    else if (moves >=18)
+        document.querySelector('ul.stars').removeChild(stars[0, 1, 2]);
+}
 
-// move counter
+// Count the number of moves and hit the getScore function to remove a star if necessary.
 function countMoves() {
     moves++;
     const moveNumber = document.querySelector('.moves');
     moveNumber.innerHTML = moves;
+    getScore();
 }
 
-const stars = document.querySelectorAll('ul.stars');
+// Start the game timer.
+function timerStart() {
+    time = 0;
+    timerId = setInterval(() => {
+        time++;
+        showTimer();
+        console.log(time);
+    }, 1000);
+}
 
-// remove stars
-function hideStars () {
-    if (moves > 2) {
-        document.querySelector('ul.stars').removeChild(stars[0]);
+// Display the timer on the page and make it human-readable.
+function showTimer () {
+    const timer = document.querySelector('.clock');
+    console.log(timer);
+    timer.innerHTML = time;
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    if (seconds < 10) {
+        timer.innerHTML = `${minutes}:0${seconds}`;
+    } else {
+        timer.innerHTML = `${minutes}:${seconds}`;
     }
 }
 
+// Resets the timer.
+function resetTimer() {
+    clearInterval(timerId);
+}
 
-// Listen for clicks
+// Core matching logic – listen for a click, and if the click target is valid, take action.
 deck.addEventListener('click', function () {
     const clickTarget = event.target;
+    // Identify list of valid click targets and exclude targets we don't want to be able to click.
     if (clickTarget.classList.contains('card') && toggledCards.length < 2 && !clickTarget.classList.contains('match') && !toggledCards.includes(clickTarget)) {
         toggleCard(clickTarget);
         addToggleCard(clickTarget);
+        // Look for a match with the toggled cards.
         if (toggledCards.length === 2) {
             findMatch(clickTarget);
             countMoves();
+        if (timerStop) {
+            timerStart();
+            timerStop = false;
+        }
         }
     }
 });
 
-// Notes here
+// Flip our cards to active.
 function toggleCard(card) {
     card.classList.toggle('open');
     card.classList.toggle('show');
 }
 
-// Notes here
+// Push the card to our toggledCards array.
 function addToggleCard(clickTarget) {
     toggledCards.push(clickTarget);
     console.log(toggledCards);
 }
 
-// Notes here
+// Search to see if we have a match.
 function findMatch () {
     if (toggledCards[0].firstElementChild.className === toggledCards[1].firstElementChild.className) {
         toggledCards[0].classList.toggle('match');
         toggledCards[1].classList.toggle('match');
         toggledCards = [];
-
     } else {
         setTimeout(function() {
             console.log('No match');
@@ -101,3 +141,11 @@ function findMatch () {
         }, 1000);
     }
 };
+
+// Show the congratulations modal if all cards are matched.
+function win() {
+    if (toggledCards == 16) {
+        timerStop();
+        popup.classList.add("show");
+    }
+}
